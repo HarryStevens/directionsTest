f40f15309467b6b82c7bf457a03b22870147fb22
HarryStevens <harryjosephstevens@gmail.com>
2014-12-02 23:04:35 -0500

    basic functionality

--- scripts.js
+++ scripts.js
@@ -4,31 +4,32 @@ var map;
 
 function initialize() {
 	directionsDisplay = new google.maps.DirectionsRenderer();
-	var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+	var newYork = new google.maps.LatLng(40.6974881, -73.979681);
 	var mapOptions = {
-		zoom : 7,
-		center : chicago
-	}
-	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+		zoom : 10,
+		center : newYork
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	directionsDisplay.setMap(map);
+	directionsDisplay.setPanel(document.getElementById("directionsPanel"));
 }
 
 function calcRoute() {
-	var start = document.getElementById("start").value;
-	var end = document.getElementById("end").value;
+	var start = document.getElementById('start').value;
+	var end = document.getElementById('end').value;
 	var request = {
 		origin : start,
 		destination : end,
 		travelMode : google.maps.TravelMode.BICYCLING
 	};
-	directionsService.route(request, function(result, status) {
+	directionsService.route(request, function(response, status) {
 		if (status == google.maps.DirectionsStatus.OK) {
-			directionsDisplay.setDirections(result);
+			directionsDisplay.setDirections(response);
 		}
 	});
 }
 
-$(document).ready(function(){
-	intialize();
-	calcRoute();
+
+$(document).ready(function() {
+	initialize();
 });
