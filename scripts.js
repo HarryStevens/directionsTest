b7430aeac406738ee924fb7bf759a4249f751581
HarryStevens <harryjosephstevens@gmail.com>
2014-12-03 00:00:50 -0500

    Basic layout mods

--- scripts.js
+++ scripts.js
@@ -1,17 +1,23 @@
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
+var windowWidth;
 
 function initialize() {
 	directionsDisplay = new google.maps.DirectionsRenderer();
 	var newYork = new google.maps.LatLng(40.6974881, -73.979681);
 	var mapOptions = {
 		zoom : 10,
-		center : newYork
+		center : newYork,
+		disableDefaultUI: true,
+		zoomControl: true,
+		zoomControlOptions: {
+			position: google.maps.ControlPosition.TOP_RIGHT
+		}
 	};
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	directionsDisplay.setMap(map);
-	directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+	directionsDisplay.setPanel(document.getElementById("directions-panel"));
 }
 
 function calcRoute() {
@@ -32,4 +38,67 @@ function calcRoute() {
 
 $(document).ready(function() {
 	initialize();
+	
+	
+	$(window).resize(function(){
+		windowWidth = $(window).width();
+		console.log("Window Width = "+windowWidth);
+		if(windowWidth<810){
+			$("#panel").css(
+				{
+					"width":"125px",
+				}
+			);
+			$(".panel-label").css(
+				{
+					"display":"block",
+					"margin-bottom":"4px"
+				}
+			);
+		}else{
+			$("#panel").css(
+				{
+					"width":"323px"
+				}
+			);
+			$(".panel-label").css(
+				{
+					"display":"inline",
+					"margin-bottom":"0px"
+				}
+			);
+		}
+	});
+	
+	$(window).load(function(){
+		windowWidth = $(window).width();
+		console.log("Window Width = "+windowWidth);
+		if(windowWidth<810){
+			$("#panel").css(
+				{
+					"width":"125px",
+				}
+			);
+			$(".panel-label").css(
+				{
+					"display":"block",
+					"margin-bottom":"4px"
+				}
+			);
+		}else{
+			$("#panel").css(
+				{
+					"width":"323px"
+				}
+			);
+			$(".panel-label").css(
+				{
+					"display":"inline",
+					"margin-bottom":"0px"
+				}
+			);
+		}
+	});
+	
+
 });
